55ff330db2f05202c6235e60f0d2067ee30206fc
csparpa <csparpa@gmail.com>
2014-10-17 12:31:15 +0100

    Introduce data aggregator class

--- lastfm_stats.py
+++ lastfm_stats.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     username = argv[1]
     client = HttpClient()
     ds = Datastore(username+'.db')
+    aggr = Aggregator(ds)
 
     def save_track_history_for(username, api_key, before=None):
         json_blob = client.get_recent_tracks_for(username, api_key, before)
@@ -23,3 +24,9 @@ if __name__ == '__main__':
     for _ in range(4):
         uts = ds.oldest_listening_uts()
         save_track_history_for(username, api_key, uts)
+
+    # Get stats
+    print 'You have listened to a total of %d tracks.' % (aggr.count_tracks(),)
+    print 'Your top 5 favorite artists: %s.' % (" ,".join(aggr.top_favorite_artists()),)
+    print 'You listen to an average of %d tracks a day.' % (aggr.daily_average_tracks(),)
+    print 'Your most active day is %s.' % (aggr.most_active_weekday(),)
---
+++ lastfm_stats/aggregator.py
@@ -0,0 +1,17 @@
+
+class Aggregator():
+
+    def __init__(self, datastore):
+        self.datastore = datastore
+
+    def count_tracks():
+        pass
+
+    def top_favorite_artists():
+        pass
+
+    def daily_average_tracks():
+        pass
+
+    def most_active_weekday():
+        pass
