d4cdfa971ec349aef9e7fa476812cac7f1bb0e8a
csparpa <csparpa@gmail.com>
2014-10-17 12:33:17 +0100

    Fix typo

--- lastfm_stats/aggregator.py
+++ lastfm_stats/aggregator.py
@@ -4,14 +4,14 @@ class Aggregator():
     def __init__(self, datastore):
         self.datastore = datastore
 
-    def count_tracks():
+    def count_tracks(self):
         pass
 
-    def top_favorite_artists():
+    def top_favorite_artists(self):
         pass
 
-    def daily_average_tracks():
+    def daily_average_tracks(self):
         pass
 
-    def most_active_weekday():
+    def most_active_weekday(self):
         pass
