9074562a72b27d036e37cc013ba83c26b4a931f4
csparpa <csparpa@gmail.com>
2014-10-18 20:50:16 +0100

    Add docs

--- lastfm_stats.py
+++ lastfm_stats.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+"""
+Lastfm_stats: aggregated Last.fm user statistics
+
+Usage:
+    python lastfm_stats.py <username>
+
+Parameters:
+    <username> = the Last.fm username you want stats for
+
+Synopsis:
+    the script will write on stdout:
+        - The total number of tracks listened by <username>
+        - The five top artists listened by <username>
+        - The daily average number of tracks listened by <username>
+        - The most active week day for <username>
+"""
+
 from lastfm_stats import UserStats
 from sys import argv
 
